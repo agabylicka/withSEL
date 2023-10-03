b3826d0bb5a3762036ba1be4621797d9999884c1
Agnieszka <aga_bylicka@poczta.onet.pl>
2023-10-03 18:22:39 +0200

    Short program to check Python version.

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def print_ver_python(actual_version):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Your Python version is {actual_version}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_ver_python(sys.version)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
