04ac0ca39e090ee97ec7ead7894cedcba7f1004a
Agnieszka <aga_bylicka@poczta.onet.pl>
2023-10-03 22:16:42 +0200

    Added assertion.

--- Selenium_tests/test1_public_urls.py
+++ Selenium_tests/test1_public_urls.py
@@ -2,12 +2,18 @@ from selenium import webdriver
 
 
 def test_google():
+    # Arrange
     # Get the driver
     driver = webdriver.Chrome()
     # Maximize window
     driver.maximize_window()
+
+    # Act
     # Get the URL
     driver.get("https://www.lambdatest.com/certifications/selenium-python-101")
     # Check response
     print(driver.title)
+
+    # Assert
+    assert(driver.title is not None)
 
